387bc37fead90308196358f50ccf51108f1ee9dc
LalithK90 <asakahatapitiya@gmail.com>
2021-01-17 10:27:19 +0530

    All html were changed

--- src/main/java/lk/sampath/autocare/asset/service_type_parameter_vehicle/entity/ServiceTypeParameterVehicle.java
+++ src/main/java/lk/sampath/autocare/asset/service_type_parameter_vehicle/entity/ServiceTypeParameterVehicle.java
@@ -2,13 +2,12 @@ package lk.sampath.autocare.asset.service_type_parameter_vehicle.entity;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
 import lk.sampath.autocare.asset.serviceTypeParameter.entity.ServiceTypeParameter;
+import lk.sampath.autocare.asset.service_type_parameter_vehicle.entity.enums.ServiceTypeParameterVehicleStatus;
 import lk.sampath.autocare.asset.vehicle.entity.Vehicle;
 import lk.sampath.autocare.util.audit.AuditEntity;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -24,5 +23,6 @@ public class ServiceTypeParameterVehicle extends AuditEntity {
   @ManyToOne
   private Vehicle vehicle;
 
-
+  @Enumerated( EnumType.STRING )
+  private ServiceTypeParameterVehicleStatus serviceTypeParameterVehicleStatus;
 }
